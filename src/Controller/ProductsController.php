950d0a9a40bca0ad8f6f88bd546eda36d613113c
Karyna Ro <karynarohozna@gmail.com>
2023-01-08 12:26:22 +0200

    added controller and endpoint

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController
+{
+    #[Route('/products/{id}/lowest-price', name: 'lowest_price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse(
+                ['error' => 'Promotions engine failure message'],
+                $request->headers->get('force_fail'));
+        }
+        return new JsonResponse([
+            'quantity' => 5,
+            "request_location" => "UK",
+            "voucher_code" => "OU812",
+            "request_date" => "2022-04-04",
+            "product_id" => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+        ], 200);
+    }
+
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions()
+    {
+//
+    }
+}
