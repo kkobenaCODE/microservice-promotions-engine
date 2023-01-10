e8db27b0a4a9be390187ddd75ce5af91a225effe
Karyna Ro <karynarohozna@gmail.com>
2023-01-10 20:23:58 +0200

    added database and logic

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,32 +2,45 @@
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiry;
+use App\Filter\PromotionsFilterInterface;
+use App\Service\Serializer\DTOSerializer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
-class ProductsController
+class ProductsController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest_price', methods: 'POST')]
-    public function lowestPrice(Request $request, int $id): Response
+    public function lowestPrice(
+        Request       $request,
+        int           $id,
+        DTOSerializer $serializer,
+    PromotionsFilterInterface $promotionsFilter): Response
     {
         if ($request->headers->has('force_fail')) {
             return new JsonResponse(
                 ['error' => 'Promotions engine failure message'],
                 $request->headers->get('force_fail'));
         }
-        return new JsonResponse([
-            'quantity' => 5,
-            "request_location" => "UK",
-            "voucher_code" => "OU812",
-            "request_date" => "2022-04-04",
-            "product_id" => $id,
-            'price' => 100,
-            'discounted_price' => 50,
-            'promotion_id' => 3,
-            'promotion_name' => 'Black Friday half price sale'
-        ], 200);
+
+        // 1) deserialize json data in EnquiryDTO
+
+        $lowestPriceEnquiry = $serializer->deserialize(
+            $request->getContent(), LowestPriceEnquiry::class, 'json'
+        );
+
+        // 2) pass the enquiry into a promotions filter
+        // the appropriate promotion will be applied
+        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry);
+
+        $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
+
+        return new Response($responseContent, 200);
+
     }
 
 
